ced72b41cb6b04973b826e4190888bb7916f9506
Greg Duckworth <2647926+gregoryduckworth@users.noreply.github.com>
2023-01-30 10:31:40 +0000

    feat: update and add new toolkit warnings (#183)

--- dist/index.js
+++ dist/index.js
@@ -4920,6 +4920,58 @@ exports.request = request;
 
 /***/ }),
 
+/***/ 2183:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+"use strict";
+
+
+const core = __nccwpck_require__(2186)
+
+/**
+ * Displays warning message if the action reference is pinned to master/main
+ */
+function logActionRefWarning() {
+  const actionRef = process.env.GITHUB_ACTION_REF
+  const repoName = process.env.GITHUB_ACTION_REPOSITORY
+
+  if (actionRef === 'main' || actionRef === 'master') {
+    core.warning(
+      `${repoName} is pinned at HEAD. We strongly ` +
+        `advise against pinning to "@${actionRef}" as it may be unstable. Please ` +
+        `update your GitHub Action YAML from:\n\n` +
+        `    uses: '${repoName}@${actionRef}'\n\n` +
+        `to:\n\n` +
+        `    uses: '${repoName}@<release/tag version>'\n\n` +
+        `Alternatively, you can pin to any git tag or git SHA in the ` +
+        `repository.`
+    )
+  }
+}
+
+/**
+ * Displays warning message if the repository is under the nearform organisation
+ */
+function logRepoWarning() {
+  const repoName = process.env.GITHUB_ACTION_REPOSITORY
+  const repoOrg = repoName.split('/')[0]
+
+  if (repoOrg != 'nearform-actions') {
+    core.warning(
+      `'${repoOrg}' is no longer a valid organisation for this action.` +
+        `Please update it to be under the 'nearform-actions' organisation.`
+    )
+  }
+}
+
+module.exports = {
+  logActionRefWarning,
+  logRepoWarning
+}
+
+
+/***/ }),
+
 /***/ 3682:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -18359,6 +18411,7 @@ function wrappy (fn, cb) {
 
 const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
+const toolkit = __nccwpck_require__(2183)
 
 const { createIssue } = __nccwpck_require__(7946)
 const { filterByColumnIds, findColumnIdByName } = __nccwpck_require__(2070)
@@ -18366,6 +18419,9 @@ const { formatCards, generateMarkdown } = __nccwpck_require__(68)
 const { getProjectBetaCards, getProjectSettings } = __nccwpck_require__(4350)
 
 const run = async () => {
+  toolkit.logActionRefWarning()
+  toolkit.logRepoWarning()
+
   core.info(`*** ACTION RUN - START ***`)
 
   try {
